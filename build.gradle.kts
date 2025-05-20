c8389c9c1a2925bc05bff8c727bff41adc6ccb48
cbist <cbist@yandex-team.ru>
2025-05-20 13:08:11 +0300

    asd

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("com.h2database:h2:2.2.224") // H2 Database (режим MySQL)
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
--- src/main/kotlin/com/example/demo/CommonController.kt
+++ src/main/kotlin/com/example/demo/CommonController.kt
@@ -2,12 +2,72 @@ package com.example.demo
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.sql.DriverManager
+import java.sql.Statement
 
 @RestController
 @RequestMapping("/api/")
 class CommonController {
-    @GetMapping("pokemon")
-    fun pokemonDetail(@RequestParam id: String): ResponseEntity<String?> {
-        return ResponseEntity.ok<String?>(id)
+
+    lateinit var statement: Statement
+
+    constructor() {
+        val connection = DriverManager.getConnection(
+            "jdbc:h2:mem:test;MODE=MySQL;DB_CLOSE_DELAY=-1",
+            "sa",
+            ""
+        )
+        statement = connection.createStatement()
+        statement.executeUpdate("CREATE TABLE IF NOT EXISTS users (name VARCHAR(50) NOT NULL, password TEXT) ENGINE=MEMORY")
+        statement.executeUpdate("INSERT INTO users (name, password) VALUES ('Kuzma', 'euo23123dew')")
+    }
+
+    @GetMapping("main/check")
+    fun mainPageCheck(
+        @RequestParam answer1: String,
+        @RequestParam answer2: String,
+        @RequestParam answer3: String,
+        @RequestParam answer4: String
+    ): ResponseEntity<String?> =
+        if (answer1 == "1ca383cd1ee7f63a" && answer2 == "b3d74eea97bfb75c" && answer3 == "26fe1629f0d72bc0" && answer4 == "c29738ef01d2060a") {
+            ResponseEntity.ok("Окак, он смог")
+        } else {
+            ResponseEntity.ok("Лох")
+        }
+
+    @GetMapping("task4/check")
+    fun task4Check(
+        @RequestParam name: String,
+        @RequestParam lName: String,
+        @RequestParam date: String,
+    ): ResponseEntity<String?> =
+        if (name == "Андрей" && lName == "Толченов") {
+            if (date == "01.07.2023") {
+                ResponseEntity.ok("Окак, он смог. 4 - c29738ef01d2060a")
+            } else if (date == "21.05.2003") {
+                ResponseEntity.ok("Обязательно твое др")
+            } else {
+                ResponseEntity.ok("А вот думай че за дата")
+            }
+        } else {
+            ResponseEntity.ok("Лох")
+        }
+
+    @GetMapping("task2/check")
+    fun task2Check(
+        @RequestParam name: String,
+        @RequestParam pass: String,
+    ): ResponseEntity<String?> {
+        var finalQuery = "SELECT * FROM users WHERE name = '$name' AND password = '$pass'"
+        var result = statement.execute(finalQuery)
+        if (result) {
+            var res = statement.resultSet
+            if (res.next())
+                return ResponseEntity.ok("Хорош, ты смог войти в аккаунт. 2 - b3d74eea97bfb75c")
+            else
+                return ResponseEntity.ok("Лох")
+        } else {
+            return ResponseEntity.ok("Лох")
+        }
     }
 }
